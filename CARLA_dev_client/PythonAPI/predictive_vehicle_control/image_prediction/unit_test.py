abf259f026e136ca3dcf90c9dbed58c0a86c5ca2
luwis93choi <luwis93choi@hotmail.com>
2021-09-14 20:28:35 +0900

    Editted unit test to include training and validation

--- CARLA_dev_client/PythonAPI/predictive_vehicle_control/image_prediction/unit_test.py
+++ CARLA_dev_client/PythonAPI/predictive_vehicle_control/image_prediction/unit_test.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import torch.optim as optim
 from torch import device
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 
 import torchvision
 from torchvision import transforms
@@ -12,6 +13,7 @@ import torchvision.transforms.functional as TF
 import cv2 as cv
 
 import numpy as np
+import datetime
 
 from tqdm import tqdm
 
@@ -19,6 +21,10 @@ from dataset_generator import carla_dataset_generator
 from dataloader import carla_dataset
 
 from UNet import UNet
+
+start_time = str(datetime.datetime.now())
+training_writer = SummaryWriter(log_dir='./runs/' + start_time + '/predictive_vehicle_control_training')
+validation_writer = SummaryWriter(log_dir='./runs/' + start_time + '/predictive_vehicle_control_validation')
 
 preprocess = transforms.Compose([
     transforms.Resize((640, 1280)),
@@ -30,15 +36,22 @@ postprocess = transforms.Compose([
 ])
 
 carla_dataset_generator = carla_dataset_generator(dataset_save_path='./carla_dataset.hdf5',
-                                                  dataset_path='/home/luwis/ICSL_Project/CARLA_NN/CARLA_dev_client/PythonAPI/automatic_dataset_collection/Recorded_Image/2021-09-08 23:50:26.834826',
+                                                  dataset_path='/home/luwis/ICSL_Project/CARLA_NN/CARLA_dev_client/PythonAPI/automatic_dataset_collection/Recorded_Image/2021-09-14 16:20:03.124257',
                                                   verbose='low')
 
-predictive_vehicle_control_dataset = carla_dataset(dataset_path='./carla_dataset.hdf5', mode='training',
-                                          train_pretransform=preprocess, train_posttransform=postprocess,
-                                          valid_pretransform=preprocess, valid_posttransform=postprocess,
-                                          test_pretransform=preprocess, test_posttransform=postprocess)
+predictive_vehicle_control_dataset_training = carla_dataset(dataset_path='./carla_dataset.hdf5', mode='training',
+                                                            train_pretransform=preprocess, train_posttransform=postprocess,
+                                                            valid_pretransform=preprocess, valid_posttransform=postprocess,
+                                                            test_pretransform=preprocess, test_posttransform=postprocess)
 
-dataloader = DataLoader(dataset=predictive_vehicle_control_dataset, batch_size=1, shuffle=True, drop_last=True)
+predictive_vehicle_control_dataset_validation = carla_dataset(dataset_path='./carla_dataset.hdf5', mode='validation',
+                                                            train_pretransform=preprocess, train_posttransform=postprocess,
+                                                            valid_pretransform=preprocess, valid_posttransform=postprocess,
+                                                            test_pretransform=preprocess, test_posttransform=postprocess)
+
+training_dataloader = DataLoader(dataset=predictive_vehicle_control_dataset_training, batch_size=16, shuffle=True, drop_last=True, num_workers=5, prefetch_factor=5, persistent_workers=False, pin_memory=False)
+
+validation_dataloader = DataLoader(dataset=predictive_vehicle_control_dataset_validation, batch_size=16, shuffle=True, drop_last=True, num_workers=5, prefetch_factor=5, persistent_workers=False, pin_memory=False)
 
 processor = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print(processor)
@@ -48,31 +61,54 @@ predictive_img_model.to(processor)
 
 learning_rate = 1e-4
 
-optimizer = optim.Adam(predictive_img_model.parameters(), lr=learning_rate)
+optimizer = optim.SGD(predictive_img_model.parameters(), lr=learning_rate, momentum=0.9, nesterov=True)
 
 criterion = nn.MSELoss()
 
-for epoch in range(10):
+for epoch in range(400):
 
     total_train_loss_list = []
+    total_valid_loss_list = []
 
+    print('Current State [Training] - [EPOCH : {}]'.format(str(epoch)))
+    predictive_img_model.train()
+    for batch_idx, (prev_img, current_img, vehicle_control_feature_vector) in enumerate(tqdm(training_dataloader)):
+
+        prev_img = prev_img.to(processor).float()
+        current_img = current_img.to(processor).float()
+        vehicle_control_feature_vector = vehicle_control_feature_vector.to(processor).float()
+
+        # print('prev_img : {}'.format(prev_img.size()))
+        # print('current_img : {}'.format(current_img.size()))
+        # print('vehicle_control_feature_vector : {}'.format(vehicle_control_feature_vector.size()))
+
+        optimizer.zero_grad()
+        reconstructed_img = predictive_img_model(prev_img, vehicle_control_feature_vector)
+        reconstruction_loss = criterion(reconstructed_img, current_img)
+        reconstruction_loss.backward()
+        optimizer.step()
+
+        total_train_loss_list.append(reconstruction_loss.item())
+
+    training_writer.add_scalar('Next Image Reconstruction Loss [{}]'.format(start_time), np.mean(total_train_loss_list), global_step=epoch)
+
+    print('Current State [Validation] - [EPOCH : {}]'.format(str(epoch)))
     predictive_img_model.eval()
+    with torch.no_grad():
 
-    for batch_idx, (prev_img, current_img, vehicle_control_feature_vector) in enumerate(tqdm(dataloader)):
+        for batch_idx, (prev_img, current_img, vehicle_control_feature_vector) in enumerate(tqdm(validation_dataloader)):
 
-        # prev_img = prev_img.to(processor).float()
-        # current_img = current_img.to(processor).float()
+            prev_img = prev_img.to(processor).float()
+            current_img = current_img.to(processor).float()
+            vehicle_control_feature_vector = vehicle_control_feature_vector.to(processor).float()
 
-        print('prev_img : {}'.format(prev_img.size()))
-        print('current_img : {}'.format(current_img.size()))
-        print('vehicle_control_feature_vector : {}'.format(vehicle_control_feature_vector.size()))
+            # print('prev_img : {}'.format(prev_img.size()))
+            # print('current_img : {}'.format(current_img.size()))
+            # print('vehicle_control_feature_vector : {}'.format(vehicle_control_feature_vector.size()))
 
-        # optimizer.zero_grad()
-        # reconstructed_img = predictive_img_model(prev_img)
-        # reconstruction_loss = criterion(reconstructed_img, current_img)
-        # reconstruction_loss.backward()
-        # optimizer.step()
+            reconstructed_img = predictive_img_model(prev_img, vehicle_control_feature_vector)
+            reconstruction_loss = criterion(reconstructed_img, current_img)
 
-        # total_train_loss_list.append(reconstruction_loss.item())
+            total_valid_loss_list.append(reconstruction_loss.item())
 
-    # print('reconstruction_loss : {}'.format(np.mean(total_train_loss_list)))
+        validation_writer.add_scalar('Next Image Reconstruction Loss [{}]'.format(start_time), np.mean(total_valid_loss_list), global_step=epoch)
