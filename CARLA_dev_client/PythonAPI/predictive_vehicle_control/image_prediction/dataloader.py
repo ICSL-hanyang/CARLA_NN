2a4a5a808b44df4fab6bf888774aa913b78648ff
luwis93choi <luwis93choi@hotmail.com>
2021-09-14 20:27:19 +0900

    Image resize / Edit vehicle control feature vector

--- CARLA_dev_client/PythonAPI/predictive_vehicle_control/image_prediction/dataloader.py
+++ CARLA_dev_client/PythonAPI/predictive_vehicle_control/image_prediction/dataloader.py
@@ -20,7 +20,7 @@ import PIL
 from PIL import Image
 
 global_dataloder_print_flag = False
-global_input_img_disp_flag = True
+global_input_img_disp_flag = False
 
 USE_GRAY = False
 
@@ -94,14 +94,19 @@ class carla_dataset(torch.utils.data.Dataset):
         ######################################################################################################
 
         ### Image Data Loading ###############################################################################
+        height = 720
+        width = 1280
+
         prev_img = cv.imread(prev_img_path)
-        prev_img = cv.resize(prev_img, (572, 572))
+        prev_img = prev_img[(height * 3)//4 : height, width//10 : (width * 9)//10, ...]
+        prev_img = cv.resize(prev_img, (120, 120))
         prev_img = np.transpose(prev_img, (2, 0, 1))
         prev_img = prev_img.astype(np.float)
         prev_img /= 255.0
 
         current_img = cv.imread(current_img_path)
-        current_img = cv.resize(current_img, (572, 572))
+        current_img = current_img[(height * 3)//4 : height, width//10 : (width * 9)//10, ...]
+        current_img = cv.resize(current_img, (120, 120))
         current_img = np.transpose(current_img, (2, 0, 1))
         current_img = current_img.astype(np.float)
         current_img /= 255.0
@@ -153,7 +158,7 @@ class carla_dataset(torch.utils.data.Dataset):
 
         ### Vehicle Control Feature Vector Assembly ###
         normalized_prev_Speed = prev_Speed / 100.0     # Normalize vehicle speed assuming that max vehicle speed as 100km/h
-        vehicle_control_feature_vector = np.array([prev_Throttle, prev_Steer, prev_Brake, normalized_prev_Speed])
+        vehicle_control_feature_vector = np.array([prev_Throttle, prev_Steer, normalized_prev_Speed, prev_Brake])
         vehicle_control_feature_vector.astype(np.float)
 
         global_print('prev_hud_data_dict keys : {}'.format(list(prev_hud_data_dict.keys())))
