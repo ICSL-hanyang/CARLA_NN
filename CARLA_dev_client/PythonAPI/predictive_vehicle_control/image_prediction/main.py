8bd4f3175fc67142cb1ba8932d164e13f4eac531
luwis93choi <luwis93choi@hotmail.com>
2021-10-14 15:54:56 +0900

    Added N frame delay dataset generator for main baseline

--- CARLA_dev_client/PythonAPI/predictive_vehicle_control/image_prediction/main.py
+++ CARLA_dev_client/PythonAPI/predictive_vehicle_control/image_prediction/main.py
@@ -19,7 +19,7 @@ import datetime
 
 from tqdm import tqdm
 
-from dataset_generator import carla_dataset_generator
+from dataset_generator import carla_dataset_generator, N_frame_delay_carla_dataset_generator
 from dataloader import carla_dataset
 
 from UNet import UNet
@@ -37,19 +37,38 @@ postprocess = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
-carla_dataset_generator = carla_dataset_generator(dataset_save_path='./carla_dataset.hdf5',
-                                                  dataset_path='/home/byungchanchoi/ICSL_Project/CARLA_NN/carla_dataset/2021-09-19 19_13_13.616261_with_segmentation/2021-09-19 19:13:13.616261_with_segmentation',
-                                                  verbose='low')
+n_frame_delay = 1
 
-predictive_vehicle_control_dataset_training = carla_dataset(dataset_path='./carla_dataset.hdf5', mode='training',
-                                                            train_pretransform=preprocess, train_posttransform=postprocess,
-                                                            valid_pretransform=preprocess, valid_posttransform=postprocess,
-                                                            test_pretransform=preprocess, test_posttransform=postprocess)
+if n_frame_delay > 1:
 
-predictive_vehicle_control_dataset_validation = carla_dataset(dataset_path='./carla_dataset.hdf5', mode='validation',
-                                                            train_pretransform=preprocess, train_posttransform=postprocess,
-                                                            valid_pretransform=preprocess, valid_posttransform=postprocess,
-                                                            test_pretransform=preprocess, test_posttransform=postprocess)
+    carla_dataset_generator = N_frame_delay_carla_dataset_generator(dataset_save_path='./carla_dataset.hdf5',
+                                                                    dataset_path='/home/luwis/ICSL_Project/CARLA_NN/CARLA_dev_client/PythonAPI/automatic_dataset_collection/Recorded_Image/2021-09-19 19:13:13.616261_with_segmentation',
+                                                                    n_frame_delay=n_frame_delay,
+                                                                    verbose='low')
+
+    predictive_vehicle_control_dataset_training = carla_dataset(dataset_path='./carla_dataset[frame_delay_' + str(n_frame_delay) + '].hdf5', mode='training',
+                                                                train_pretransform=preprocess, train_posttransform=postprocess,
+                                                                valid_pretransform=preprocess, valid_posttransform=postprocess,
+                                                                test_pretransform=preprocess, test_posttransform=postprocess)
+
+    predictive_vehicle_control_dataset_validation = carla_dataset(dataset_path='./carla_dataset[frame_delay_' + str(n_frame_delay) + '].hdf5', mode='validation',
+                                                                train_pretransform=preprocess, train_posttransform=postprocess,
+                                                                valid_pretransform=preprocess, valid_posttransform=postprocess,
+                                                                test_pretransform=preprocess, test_posttransform=postprocess)
+else:
+    carla_dataset_generator = carla_dataset_generator(dataset_save_path='./carla_dataset.hdf5',
+                                                      dataset_path='/home/luwis/ICSL_Project/CARLA_NN/CARLA_dev_client/PythonAPI/automatic_dataset_collection/Recorded_Image/2021-09-19 19:13:13.616261_with_segmentation',
+                                                      verbose='low')
+                                                    
+    predictive_vehicle_control_dataset_training = carla_dataset(dataset_path='./carla_dataset.hdf5', mode='training',
+                                                                train_pretransform=preprocess, train_posttransform=postprocess,
+                                                                valid_pretransform=preprocess, valid_posttransform=postprocess,
+                                                                test_pretransform=preprocess, test_posttransform=postprocess)
+
+    predictive_vehicle_control_dataset_validation = carla_dataset(dataset_path='./carla_dataset.hdf5', mode='validation',
+                                                                train_pretransform=preprocess, train_posttransform=postprocess,
+                                                                valid_pretransform=preprocess, valid_posttransform=postprocess,
+                                                                test_pretransform=preprocess, test_posttransform=postprocess)
 
 training_dataloader = DataLoader(dataset=predictive_vehicle_control_dataset_training, batch_size=16, shuffle=True, drop_last=True, num_workers=5, prefetch_factor=5, persistent_workers=False, pin_memory=False)
 
